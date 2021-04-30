019e5dce3f8d8bedbf599966e354d379023a7243
Ian Goodnight <goodnight.ian@gmail.com>
2021-04-30 16:58:24 -0400

    still need to expand unit testing

--- app.js
+++ app.js
@@ -2,7 +2,7 @@
 
 /*
  * Slapfiliate is a utility that will fetch active balances from tapfilliate,
- * the member IDs associated, create a patch payout via paypal, and update the
+ * the member IDs associated, create a batch payout via paypal, and update the
  * active balances back in tapfilliate.  As it is financial in nature, it should
  * also be logged somewhere/somehow.
  */
@@ -17,22 +17,30 @@ const {
   postPayments,
 } = require('./lib/tapfiliate');
 const { postBatchPayout } = require('./lib/paypal');
-const { applyPayFloor, injectSandBoxAffiliates } = require('./lib/utils');
+const {
+  applyPayFloor,
+  colorConsole: code,
+  injectSandBoxAffiliates,
+} = require('./lib/utils');
 
 (function settleBalances() {
-  console.log('Preparing to settle tapfiliate balances...');
+  const label = 'Time taken to settle tapfiliate balances';
+  console.time(label);
+  console.log(code.green, 'Preparing to settle tapfiliate balances...');
   getBalances()
     .then(applyPayFloor)
     .then(getPayoutMethods)
-    .then(injectSandBoxAffiliates) // inject test accounts
+    .then(injectSandBoxAffiliates) // if not production, inject sandbox
     .then(postBatchPayout)
     .then(postPayments)
     .then((affiliates) => {
       affiliates.forEach((entry) => {
         console.log(entry);
       });
+      console.timeEnd(label);
     })
     .catch((err) => {
-      console.log(err);
+      console.error(code.red, err);
+      console.timeEnd(label);
     });
 })();
